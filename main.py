c4365998518376c6ee58da6f19d0dcd9e13eacd5
LeahB13 <balhousl@students.dsbn.org>
2025-03-24 12:34:31 -0400

    Random Number Average

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+# Name: Leah Balhous
+# Title: Random Number Average
+# Date: March 24th, 2025
+# Description: This program gets the average of 100 random numbers
 
+import random
+
+numbers = []
+for i in range(100) :
+    num = random.randint(0, 100)
+    numbers.append(num)
+print(sum(numbers)/100)
